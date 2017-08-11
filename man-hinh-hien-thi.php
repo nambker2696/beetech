d7be5edff772f3ae895a44f6d47e224b2cb85309
toanmx0101 <wth8647@gmail.com>
2017-08-11 18:51:41 +0700

    fix header man hinh hien thi

--- man-hinh-hien-thi.php
+++ man-hinh-hien-thi.php
@@ -6,6 +6,7 @@
   <title>Màn hình đăng nhập</title>
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/header.css">
   <link rel="stylesheet" href="css/man-hinh-hien-thi.css">
   <link rel="stylesheet" type="text/css" href="dist/semantic.min.css">
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" />
@@ -22,7 +23,7 @@
   </script>
 </head>
 <body>
-  <?php include('header-chu-tau.php') ?>
+  <?php include('header-logged.php') ?>
   <div class="container">
     <div class="content" style="display: flex;">
       <div class="calendar">
