cf525f996a6fa9095471200732b17dd91fc5efa7
unknown <nguyenvannam2013696@gmail.com>
2017-08-14 18:56:48 +0700

    home

--- home.php
+++ home.php
@@ -48,19 +48,141 @@
 		<div class="container">
 			<h1>Du lịch Việt Nam</h1>
 			<div class="row">
-				<div class="col-md-3 travel-image">
-					<img src="img/1.jpg" alt="">
-
+				<div class="col-md-3">
+					<div class="travel-image">
+						<div class="product-card">
+							<img src="img/2.jpg" alt="">
+							<div class="product-content-wrapper">
+								<a class="category-name" href="/category/171" data-reactid="656">Attractions</a>
+								<a class="product-title" href="/tour/2920/france/paris/skip-the-line-catacombs-tickets-with-audioguide" data-reactid="657">Skip the Line Catacombs Tickets with Audioguide</a>
+								<div class="product-detail" data-reactid="658">
+									<div class="product-detail-left" data-reactid="659">
+										<div class="product-review-wrapper" data-reactid="660">
+											<div class="product-card-rating-wrapper " data-reactid="661">
+												<div class="product-rating" data-reactid="662"><span data-reactid="663">4.7</span>
+												</div><i class="fa fa-star star-svg" aria-hidden="true"></i>
+											</div>
+											<div class="product-review-count" data-reactid="666"><span data-reactid="667">(135 Ratings)</span>
+											</div>
+										</div>
+										<div class="call-to-action" data-reactid="668"><svg xmlns="http://www.w3.org/2000/svg" class="bell-svg" data-reactid="669"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/dist/svg/feed-page-RxcEvkMV.svg#notifications-icon" data-reactid="670"></use></svg><span data-reactid="671">11 views in the last 2 hours</span>
+										</div>
+									</div>
+									<div class="product-detail-right" data-reactid="672">
+										<div class="product-from-text" data-reactid="673"><span data-reactid="674">from</span>
+										</div>
+										<div class="product-cut-price" data-reactid="675"><span data-reactid="676"></span>
+										</div>
+										<div class="product-price" data-reactid="677"><span data-reactid="678">€30</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<a href="index.php" title=""><i class="fa fa-heart-o fa-2x heart-svg" aria-hidden="true"></i></a>
+						</div>
+					</div>
 				</div>
-				<div class="col-md-3 travel-image">
-					<img src="img/1.jpg" alt="">
+				<div class="col-md-3">
+					<div class="travel-image">
+						<div class="product-card">
+							<img src="img/2.jpg" alt="">
+							<div class="product-content-wrapper">
+								<a class="category-name" href="/category/171" data-reactid="656">Attractions</a>
+								<a class="product-title" href="/tour/2920/france/paris/skip-the-line-catacombs-tickets-with-audioguide" data-reactid="657">Skip the Line Catacombs Tickets with Audioguide</a>
+								<div class="product-detail" data-reactid="658">
+									<div class="product-detail-left" data-reactid="659">
+										<div class="product-review-wrapper" data-reactid="660">
+											<div class="product-card-rating-wrapper " data-reactid="661">
+												<div class="product-rating" data-reactid="662"><span data-reactid="663">4.7</span>
+												</div><i class="fa fa-star star-svg" aria-hidden="true"></i>
+											</div>
+											<div class="product-review-count" data-reactid="666"><span data-reactid="667">(135 Ratings)</span>
+											</div>
+										</div>
+										<div class="call-to-action" data-reactid="668"><svg xmlns="http://www.w3.org/2000/svg" class="bell-svg" data-reactid="669"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/dist/svg/feed-page-RxcEvkMV.svg#notifications-icon" data-reactid="670"></use></svg><span data-reactid="671">11 views in the last 2 hours</span>
+										</div>
+									</div>
+									<div class="product-detail-right" data-reactid="672">
+										<div class="product-from-text" data-reactid="673"><span data-reactid="674">from</span>
+										</div>
+										<div class="product-cut-price" data-reactid="675"><span data-reactid="676"></span>
+										</div>
+										<div class="product-price" data-reactid="677"><span data-reactid="678">€30</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<a href="index.php" title=""><i class="fa fa-heart-o fa-2x heart-svg" aria-hidden="true"></i></a>
+						</div>
+					</div>
 				</div>
-				<div class="col-md-3 travel-image">
-					<img src="img/1.jpg" alt="">
+				<div class="col-md-3">
+					<div class="travel-image">
+						<div class="product-card">
+							<img src="img/2.jpg" alt="">
+							<div class="product-content-wrapper">
+								<a class="category-name" href="/category/171" data-reactid="656">Attractions</a>
+								<a class="product-title" href="/tour/2920/france/paris/skip-the-line-catacombs-tickets-with-audioguide" data-reactid="657">Skip the Line Catacombs Tickets with Audioguide</a>
+								<div class="product-detail" data-reactid="658">
+									<div class="product-detail-left" data-reactid="659">
+										<div class="product-review-wrapper" data-reactid="660">
+											<div class="product-card-rating-wrapper " data-reactid="661">
+												<div class="product-rating" data-reactid="662"><span data-reactid="663">4.7</span>
+												</div><i class="fa fa-star star-svg" aria-hidden="true"></i>
+											</div>
+											<div class="product-review-count" data-reactid="666"><span data-reactid="667">(135 Ratings)</span>
+											</div>
+										</div>
+										<div class="call-to-action" data-reactid="668"><svg xmlns="http://www.w3.org/2000/svg" class="bell-svg" data-reactid="669"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/dist/svg/feed-page-RxcEvkMV.svg#notifications-icon" data-reactid="670"></use></svg><span data-reactid="671">11 views in the last 2 hours</span>
+										</div>
+									</div>
+									<div class="product-detail-right" data-reactid="672">
+										<div class="product-from-text" data-reactid="673"><span data-reactid="674">from</span>
+										</div>
+										<div class="product-cut-price" data-reactid="675"><span data-reactid="676"></span>
+										</div>
+										<div class="product-price" data-reactid="677"><span data-reactid="678">€30</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<a href="index.php" title=""><i class="fa fa-heart-o fa-2x heart-svg" aria-hidden="true"></i></a>
+						</div>
+					</div>
 				</div>
-				<div class="col-md-3 travel-image">
-					<img src="img/1.jpg" alt="">
-					
+				<div class="col-md-3">
+					<div class="travel-image">
+						<div class="product-card">
+							<img src="img/2.jpg" alt="">
+							<div class="product-content-wrapper">
+								<a class="category-name" href="/category/171" data-reactid="656">Attractions</a>
+								<a class="product-title" href="/tour/2920/france/paris/skip-the-line-catacombs-tickets-with-audioguide" data-reactid="657">Skip the Line Catacombs Tickets with Audioguide</a>
+								<div class="product-detail" data-reactid="658">
+									<div class="product-detail-left" data-reactid="659">
+										<div class="product-review-wrapper" data-reactid="660">
+											<div class="product-card-rating-wrapper " data-reactid="661">
+												<div class="product-rating" data-reactid="662"><span data-reactid="663">4.7</span>
+												</div><i class="fa fa-star star-svg" aria-hidden="true"></i>
+											</div>
+											<div class="product-review-count" data-reactid="666"><span data-reactid="667">(135 Ratings)</span>
+											</div>
+										</div>
+										<div class="call-to-action" data-reactid="668"><svg xmlns="http://www.w3.org/2000/svg" class="bell-svg" data-reactid="669"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/dist/svg/feed-page-RxcEvkMV.svg#notifications-icon" data-reactid="670"></use></svg><span data-reactid="671">11 views in the last 2 hours</span>
+										</div>
+									</div>
+									<div class="product-detail-right" data-reactid="672">
+										<div class="product-from-text" data-reactid="673"><span data-reactid="674">from</span>
+										</div>
+										<div class="product-cut-price" data-reactid="675"><span data-reactid="676"></span>
+										</div>
+										<div class="product-price" data-reactid="677"><span data-reactid="678">€30</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<a href="index.php" title=""><i class="fa fa-heart-o fa-2x heart-svg" aria-hidden="true"></i></a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
